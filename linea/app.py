7913f7b8a8e9d98ccc7d53a955eb21e94de5a466
mmerph <mercedes160503@gmail.com>
2025-01-27 10:58:06 -0700

    Agregué argparse para usar argumantos m y b

--- linea/app.py
+++ linea/app.py
@@ -1,16 +1,22 @@
 # Cálculo de coordenadas de líneas
 import funciones
+import argparse
 
-def main():
-    m = 2.0
-    b = 3.0
+def main(m: float, b: float):
     '''
-    X = [x for x in range(1,11)]
-    Y = [funciones.calcular_y(x, m , b) for x in X]
-    print("Enteros: ")
-    coordenadas_enteros = list(zip (X,Y))
-    print(coordenadas_enteros)
-'''
+    Función principal qu e calcula las coordenadas de una línea recta.
+    Recibimos m y b 
+    Regresa: nada   
+    '''   
+    # m = 2.0
+    # b = 3.0
+    
+    # X = [x for x in range(1,11)]
+    # Y = [funciones.calcular_y(x, m , b) for x in X]
+    # print("Enteros: ")
+    # coordenadas_enteros = list(zip (X,Y))
+    # print(coordenadas_enteros)
+
     XF = [x/10.0 for x in range(10,110,5)]
     YF = [funciones.calcular_y(x, m , b) for x in XF]
     print("Flotantes: ")
@@ -18,4 +24,11 @@ def main():
     print(coordenadas_flotantes)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', type = float,
+    help = 'Pendiente de la línea', default = 2.0)
+    parser.add_argument('-b', type = float,
+    help = 'Ordenada al origen' , default = 3.0)
+    args = parser.parse_args()
+    main(args.m, args.b)
+    #main(m=2.0, b=3.0)
