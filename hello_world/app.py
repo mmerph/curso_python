5424743262f654ff0e96e92e67cb9b183735df4a
mmerph <mercedes160503@gmail.com>
2025-03-05 10:54:21 -0700

    hicimos una página web con html en el código de python, para ver el indice y una página about

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,12 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/") #home or root of website
+@app.route("/") # home or root of website
 def index():
-    return '<html><head><title>HELLO WORLD</title></head></html><h1>Hello world</h1><p>Ir a<a href = "/about">about</a></p></body></html>'
-@app.route("/about") #info about this site
+    return '''<html>
+                    <head>
+                        <title>HELLO WORLD</title>
+                    </head>
+                    <body>
+                    <h1>Hello World</h1>
+                        <p>Ir a <a href="/about">Acerca de</a></p>
+                    </body>
+            </html>'''
+
+@app.route("/about") # info about this site
 def about():
-    '<html><head><title>About this page </title></head><body> Everything about this website. Back to <a href="/"> Hello World </a></body></html>'
- 
-if __name__ == '__main__':
+    return '''<html>
+                    <head>
+                        <title>About this page</title>
+                    </head>
+                    <body>  
+                        <p>Ir a la página de <a href="/">Inicio</a><p>
+                    </body>
+    </html>'''
+
+if __name__ == "__main__":
     app.run(debug=True)
