3314d126e4cd2698aa3d35861b8077e774407da2
mmerph <mercedes160503@gmail.com>
2025-03-06 10:34:55 -0700

    Creamos la funcion busca_en_titulo()

--- libros_web/funciones.py
+++ libros_web/funciones.py
@@ -3,14 +3,28 @@ import csv
 
 def lee_archivo_csv(archivo: str)-> list:
     '''Lee un archivo CSV y lo convierte en una lista de diccionarios'''
-    with open(archivo, "r", eencoding= 'utf8') as f:
+    with open(archivo, "r", encoding= 'utf8') as f:
         return [x for x in csv.DictReader(f)]
     
 def crea_diccoinario_titulos(lista:list)->dict:
     '''Crea un diccionario con los títulos como clave y el resto de los datos como valores'''
-    return {x['Titulo']: x for x in lista}
+    return {x['title']: x for x in lista}
+
+def busca_en_titulo(diccionario, palabra)-> list:
+    '''Busca palabra en titulo de la lista de diccionarios'''
+    lista =[]
+    palabra = palabra.lower()
+    for titulo, libro in diccionario.items():
+        if palabra in titulo.lower():
+            lista.append(libro)
+    return lista
+
+
 
 if __name__ == '__main__':
     archivo_csv = 'booklist2000.csv'
     lista_libros = lee_archivo_csv(archivo_csv)
     diccionario_libros = crea_diccoinario_titulos(lista_libros)
+    
+    resultado = busca_en_titulo(diccionario_libros, 'rebel')
+    print(resultado)
